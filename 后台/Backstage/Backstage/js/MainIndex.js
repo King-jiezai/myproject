7a931fdde3c48384c0ed7669edca90bc01fdfe2b
King-jiezai <40854679+King-jiezai@users.noreply.github.com>
2018-09-22 18:01:54 +0800

    后台新代码

---
+++ 后台/Backstage/Backstage/js/MainIndex.js
@@ -0,0 +1,40 @@
+var myChart = echarts.init(document.getElementById('main'))
+myChart.setOption({
+    color: ['#3398DB'],
+    tooltip : {
+        trigger: 'axis',
+        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis : [
+        {
+            type : 'category',
+            data :this.reg,
+            axisTick: {
+                alignWithLabel: true
+            }
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value'
+        }
+    ],
+    series : [
+        {
+            name:'网站访问',
+            type:'bar',
+            radius: [0, '50%'],
+            center:['35%','50%'],
+            barWidth: '60%',
+            data: this.res 
+        }
+    ]
+})
